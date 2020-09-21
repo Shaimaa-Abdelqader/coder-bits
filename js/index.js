e0a379ce8887fdeafe0f8570bbe9f54cb745391f
Shaimaa-Abdelqader <[shaimaa.abdelqader7@gmail.com]>
2020-09-21 08:43:55 +0200

    addding javascript file

---
+++ js/index.js
@@ -0,0 +1,21 @@
+$('.owl-carousel').owlCarousel({
+    margin:10,
+    nav:true,
+    items:3,
+    loop:true,
+    margin:10,
+    autoplay:true,
+    autoplayTimeout:4000,
+    autoplayHoverPause:false,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:1
+        },
+        1000:{
+            items:2
+        }
+    }
+})
